62307eac9e02458c63c6f7ff7d24bceba4e17df5
Bruno <brunosilva.dev@outlook.com>
2019-11-07 18:54:56 -0300

    Request Alteracao

--- Projeto1-Crud/RestApi/Model/PortifolioModel.js
+++ Projeto1-Crud/RestApi/Model/PortifolioModel.js
@@ -19,8 +19,12 @@ module.exports = class PortifolioModel {
         );
     }
 
-    static deletar(Id, callback){
+    static deletar(Id, callback) {
         return db.query("DELETE FROM PORTIFOLIO WHERE ID_PORTIFOLIO = ?", [Id], callback);
     }
 
+    static editar(portifolio, callback) {
+        return db.query("UPDATE PORTIFOLIO SET DESCRICAO = ?, DETALHES = ? WHERE ID_PORTIFOLIO = ?", 
+        [portifolio.descricao, portifolio.detalhes, portifolio.id], callback);
+    }
 }
--- Projeto1-Crud/RestApi/Router/PortifolioRouter.js
+++ Projeto1-Crud/RestApi/Router/PortifolioRouter.js
@@ -19,7 +19,7 @@ router.get('/', (req, res, next) => { // Requisição, Resposta e Next
     });
 })
 
-router.get('/:id?', (req, res, next) => { // Requisição, Resposta e Next
+router.get('/:id?', (req, res, next) => {
 
     PortifolioModel.getId(req.params.id, function (erro, retorno) {
         let resposta = new RespostaClass();
@@ -34,7 +34,7 @@ router.get('/:id?', (req, res, next) => { // Requisição, Resposta e Next
     });
 })
 
-router.post('/?', (req, res, next) => { // Requisição, Resposta e Next
+router.post('/?', (req, res, next) => {
 
     PortifolioModel.adicionar(req.body, function (erro, retorno) {
         let resposta = new RespostaClass();
@@ -47,8 +47,7 @@ router.post('/?', (req, res, next) => { // Requisição, Resposta e Next
             if (retorno.affectedRows > 0) { // Quantas linhas foram afetadas
                 resposta.msg = "Cadastro realizado com Sucesso";
             }
-            else
-            {
+            else {
                 resposta.erro = true;
                 resposta.msg = "Não foi possivel realizar o Cadastro";
             }
@@ -58,7 +57,7 @@ router.post('/?', (req, res, next) => { // Requisição, Resposta e Next
     });
 })
 
-router.delete('/:id', (req, res, next) => { // Requisição, Resposta e Next
+router.delete('/:id', (req, res, next) => {
 
     PortifolioModel.deletar(req.params.id, function (erro, retorno) {
         let resposta = new RespostaClass();
@@ -71,10 +70,32 @@ router.delete('/:id', (req, res, next) => { // Requisição, Resposta e Next
             if (retorno.affectedRows > 0) { // Quantas linhas foram afetadas
                 resposta.msg = "Portifolio excuido com sucesso";
             }
-            else
-            {
+            else {
                 resposta.erro = true;
                 resposta.msg = "Não foi possivel Excluir o Portifolio";
+            }
+        }
+        console.log('erro:', resposta);
+        res.json(resposta);
+    });
+})
+
+router.put('/', (req, res, next) => {
+
+    PortifolioModel.editar(req.body, function (erro, retorno) {
+        let resposta = new RespostaClass();
+        if (erro) {
+            resposta.erro = true;
+            resposta.msg = "Ocorreu um erro";
+            console.log('erro: ', erro);
+        }
+        else {
+            if (retorno.affectedRows > 0) { // Quantas linhas foram afetadas
+                resposta.msg = "Portifolio Alterar com sucesso";
+            }
+            else {
+                resposta.erro = true;
+                resposta.msg = "Não foi possivel Alterar o Portifolio";
             }
         }
         console.log('erro:', resposta);
